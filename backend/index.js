ea127a9815721505abd71802ddb4b376eebf67a0
Shreya Garg <shreyag.garg72@gmail.com>
2024-09-23 18:33:08 +0530

    node js with mongoose configured

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 dotenv.config();
 
 const app = express();
+connectDB();
 const port = process.env.PORT || 5000;
 
 app.listen(process.env.PORT,()=>{
     console.log("Server started on port 5000")
 })
+
