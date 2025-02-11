0f5df2f483239ad8b3eb81cc04fafbbdc2887267
Shreya Garg <shreyag.garg72@gmail.com>
2025-02-11 15:55:07 +0530

    config removed from loginsignup

--- frontend/src/components/LoginSignupModel.jsx
+++ frontend/src/components/LoginSignupModel.jsx
@@ -210,18 +210,8 @@ import {
   signInWithPopup
 } from "firebase/auth";
 import { toast } from "react-toastify";
+import { auth } from "../../firebaseConfig";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 const LoginSignupModel = ({ showModal, closeModal }) => {
